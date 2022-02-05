da4188c712d73d90814bf784de0fad173b6fe336
Lilianada <lilibest30.lb@gmail.com>
2022-02-05 12:47:27 +0100

    clean up folder structure

---
+++ src/components/ClientList/ClientList.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './ClientList.css'
+
+export default function Clients (){
+    return(
+        <section className='clientSection'>
+            <h2>Our clients</h2>
+            <ul class="logosList">
+                <li className='logoLink'><img src={"https://3yfejk2clwu33spmwz2ehh1f-wpengine.netdna-ssl.com/wp-content/themes/noformat/pictures/coca-cola.png"}/></li>
+                <li className='logoLink'><img src={"https://3yfejk2clwu33spmwz2ehh1f-wpengine.netdna-ssl.com/wp-content/themes/noformat/pictures/hersheys.png"}/></li>
+                <li className='logoLink'><img src={"https://3yfejk2clwu33spmwz2ehh1f-wpengine.netdna-ssl.com/wp-content/themes/noformat/pictures/capital-one.png"}/></li>
+                <li className='logoLink'><img src={"https://3yfejk2clwu33spmwz2ehh1f-wpengine.netdna-ssl.com/wp-content/themes/noformat/pictures/levis.png"}/></li>
+                <li className='logoLink'><img src={"https://3yfejk2clwu33spmwz2ehh1f-wpengine.netdna-ssl.com/wp-content/themes/noformat/pictures/nike.png"}/></li>
+                <li className='logoLink'><img src={"https://3yfejk2clwu33spmwz2ehh1f-wpengine.netdna-ssl.com/wp-content/themes/noformat/pictures/mtv.png"}/></li>
+                <li className='logoLink'><img src={"https://3yfejk2clwu33spmwz2ehh1f-wpengine.netdna-ssl.com/wp-content/themes/noformat/pictures/fox.png"}/></li>
+                <li className='logoLink'><img src={"https://3yfejk2clwu33spmwz2ehh1f-wpengine.netdna-ssl.com/wp-content/themes/noformat/pictures/wsj.png"}/></li>
+                <li className='logoLink'><img src={"https://3yfejk2clwu33spmwz2ehh1f-wpengine.netdna-ssl.com/wp-content/themes/noformat/pictures/special-olympics.png"}/></li>
+                <li className='logoLink'><img src={"https://3yfejk2clwu33spmwz2ehh1f-wpengine.netdna-ssl.com/wp-content/themes/noformat/pictures/logitec.png"}/></li>
+                <li className='logoLink'><img src={"https://3yfejk2clwu33spmwz2ehh1f-wpengine.netdna-ssl.com/wp-content/themes/noformat/pictures/the-wall-street-journal.png"}/></li>
+                <li className='logoLink'><img src={"https://3yfejk2clwu33spmwz2ehh1f-wpengine.netdna-ssl.com/wp-content/themes/noformat/pictures/nbc.png"}/></li>
+                <li className='logoLink'><img src={"https://3yfejk2clwu33spmwz2ehh1f-wpengine.netdna-ssl.com/wp-content/themes/noformat/pictures/acura.png"}/></li>
+                <li className='logoLink'><img src={"https://3yfejk2clwu33spmwz2ehh1f-wpengine.netdna-ssl.com/wp-content/themes/noformat/pictures/sony.png"}/></li>
+                <li className='logoLink'><img src={"https://3yfejk2clwu33spmwz2ehh1f-wpengine.netdna-ssl.com/wp-content/themes/noformat/pictures/ebay.png"} /></li>
+                <li className='logoLink'><img src={"https://3yfejk2clwu33spmwz2ehh1f-wpengine.netdna-ssl.com/wp-content/themes/noformat/pictures/nisan.png"}/></li>
+                <li className='logoLink'><img src={"https://3yfejk2clwu33spmwz2ehh1f-wpengine.netdna-ssl.com/wp-content/themes/noformat/pictures/bank-of-america.png"}/></li>
+                <li className='logoLink'><img src={"https://3yfejk2clwu33spmwz2ehh1f-wpengine.netdna-ssl.com/wp-content/themes/noformat/pictures/virgin.png"}/></li>
+            </ul>
+        </section>
+    );
+}
